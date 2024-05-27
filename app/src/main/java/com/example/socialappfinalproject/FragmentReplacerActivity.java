4c18d4c978d385029c6247b98b561e122ceb901e
hnghao11 <21110768@student.hcmute.edu.vn>
2024-05-27 20:52:47 +0700

    first commit

---
+++ app/src/main/java/com/example/socialappfinalproject/FragmentReplacerActivity.java
@@ -0,0 +1,14 @@
+package com.example.socialappfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FragmentReplacerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_replacer);
+    }
+}
